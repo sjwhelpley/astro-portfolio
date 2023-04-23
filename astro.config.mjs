12eb97ab05a0c16c64f28fd671c52df471e99d8d
Samantha Whelpley <sjwhelpley@hotmail.com>
2023-04-23 13:03:14 -0400

    Updated blog styling, added first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: 'https://samwhelpley.me',
+  integrations: [react(), mdx()],
+  site: 'https://samwhelpley.me'
 });
